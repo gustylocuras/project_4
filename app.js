da08366dd0f0dcba7c62b95b4125a45676d2c8b2
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-07-08 09:29:38 -0700

    front end started

--- app.js
+++ app.js
@@ -1,1 +1,104 @@
-console.log('hello world');
+class App extends React.Component {
+  state = {
+    pets: []
+  }
+
+  componentDidMount = () => {
+  axios.get('/pets').then((response) => {
+    this.setState({
+      pets: response.data
+    })
+  })
+}
+
+createPet = (event) => {
+  event.preventDefault()
+  axios.post('/pets',
+{
+  name: this.state.newPetName,
+  image: this.state.newPetImage,
+  description: this.state.newPetDescription
+}).then(
+    (response) => {
+      this.setState({
+        pets:response.data
+      })
+    }
+  )
+}
+
+changeNewPetName = (event) => {
+  this.setState({
+    newPetName: event.target.value
+  })
+}
+
+changeNewPetImage = (event) => {
+  this.setState({
+    newPetImage: event.target.value
+  })
+}
+
+changeNewPetDescription = (event) => {
+  this.setState({
+    newPetDescription: event.target.value
+  })
+}
+
+
+  render = () => {
+    return <div>
+          <h2>Post Pet</h2>
+            <form  onSubmit={this.createPet}>
+              <input onKeyUp={this.changeNewPetName} type="text" placeholder="name"/><br/>
+              <input onKeyUp={this.changeNewPetImage} type="text" placeholder="image url"/><br/>
+              <input onKeyUp={this.changeNewPetDescription} type="text" placeholder="description"/><br/>
+              <input type="submit" value="Post Pet"/>
+            </form>
+            <h2>Pets for adoption</h2>
+            <div className="allPets">
+            {this.state.pets.map((pet, i) => {
+              return <Pets pets={pet} key={i}  />
+            })}
+
+            </div>
+            </div>
+
+  }
+}
+
+class Pets extends React.Component{
+  state = {
+    adopted: false
+  }
+
+  toggleShowPet = () => {
+    this.setState({
+      adopted: !this.state.adopted
+    })
+  }
+
+  render = () => {
+    return <div>{this.props.pets.name}</div>
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+  <App></App>,
+  document.querySelector('main')
+)
